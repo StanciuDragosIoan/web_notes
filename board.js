6b5779489d1a888d7ccdde4680ac459f08316365
stanciudragosioan <stanciudragosioan92@gmail.com>
2020-07-11 01:35:10 +0300

    node commit

---
+++ board.js
@@ -0,0 +1,34 @@
+/*
+
+1 point = 30 minutes (can do 1 or multiple in succession)
+1 point = 30 minutes of actual work not accomplishment, if 
+          task is not done after the points have been spent
+          that's ok, just monitor overall progress
+
+### Tasks to do ###       ### Points ###
+
+Revision NodeJS 2         1
+NodeJS Course             5
+React Course              5
+Revision React            1
+Docker                    1
+
+
+### Task In Progress      
+
+NodeJS Course
+
+
+
+### Tasks Done          ### Points burned
+
+Revision NodeJS 1            1.2
+
+NodeJS Course                 2
+
+### Total points covered between July 11 - July 20 (9 days)
+
+
+
+
+*/
