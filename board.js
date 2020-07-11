7600621ac7dab6f2f60c4c07f60e8d9cb89f7b69
stanciudragosioan <stanciudragosioan92@gmail.com>
2020-07-11 14:45:21 +0300

    React notes

--- board.js
+++ board.js
@@ -12,21 +12,22 @@ NodeJS Course             5
 React Course              5
 Revision React            1
 Docker                    1
+Electron app              1
+Blog                      1
+JS Course                 5
 
 
 ### Task In Progress      
 
-NodeJS Course
+React Course
 
 
 
-### Tasks Done          ### Points burned
+### Tasks Done   ### Points burned  ### Actual coursetime      ### efficiency
 
-Revision NodeJS 1            1.2
+React Course               2              180 min                   300%  
 
-NodeJS Course                 2
-
-### Total points covered between July 11 - July 20 (9 days)
+### Total points covered between July 11 - July 21 (10 days)
 
 
 
